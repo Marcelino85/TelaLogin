6382bc88703594a90c3a4c18da64d436e43e5fcb
Marcelino <marcelino85albuquerque@gmail.com>
2024-03-21 18:43:39 -0300

    estilizando a interface

--- src/assets/components/Button/style.ts
+++ src/assets/components/Button/style.ts
@@ -4,10 +4,10 @@ import styled from 'styled-components';
 export const ButtonContainer = styled.button`
     width: 100%;
     height: 42px;
-    background-color: #81259D;
+    background-color: #03A9F4;
     color: #FFF;
 
-    border: 1px solid #81259D;
+    border: 1px solid #03A9F4;
     border-radius: 21px;
 
     &:hover {
--- src/assets/pages/Login/styles.ts
+++ src/assets/pages/Login/styles.ts
@@ -15,6 +15,7 @@ export const LoginContainer = styled.div`
     width: 100%;
     max-width: 420px;
     background-color: #FFFFFF;
+    border:1px solid #03A9F4;
 
     border-radius: 8px;
     padding: 20px;
@@ -28,7 +29,7 @@ export const LoginContainer = styled.div`
 export const Title = styled.h1`
     font-size: 42px;
     font-weight: 700;
-    color: #81259D;
+    color: #03A9F4;
 `
 
 export const Row = styled.div`
